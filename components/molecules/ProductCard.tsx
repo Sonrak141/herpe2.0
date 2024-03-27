ffedc02f0f2d4bce6d0f9419867ff80ba0ca0f8b
Carlos Borrero <53616525+Sonrak141@users.noreply.github.com>
2024-03-27 08:56:34 -0500

    Card text

--- components/molecules/ProductCard.tsx
+++ components/molecules/ProductCard.tsx
@@ -10,7 +10,7 @@ function ProductCard({ name, image, category }: Product) {
   return (
     <div className=" bg-yellow-200 bg-opacity-25 shadow-lg backdrop-filter backdrop-blur-lg border-1 border-white border-opacity-18 rounded-[1rem]  p-10 md:min-h-[18rem] min-h-[20rem] md:max-h-[18rem] max-h-[20rem] m-5 max-w-[18rem] min-w-[18rem]">
       <Image src={image} alt={name} width={80} height={80} className='mx-auto w-auto h-auto' />
-      <h6 className='text-[484848] mt-10'><b>{name}</b></h6>
+      <h6 className='text-[484848] '><b>{name}</b></h6>
       {/* <small className='text-[484848]'>{category}</small> */}
     </div>
   )
